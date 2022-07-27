b06817305a7d449ac065592678ddc76e3587f7e5
claudiostn <claudiostn26@gmail.com>
2022-07-27 20:50:06 -0300

    exercicio 2 de formulário

---
+++ curso-em-video/modulo4/ex025/cadastro.php
@@ -0,0 +1,10 @@
+Os dados foram enviados! 
+Aprenda php para saber o que fazer com eles.
+
+CONTROLES DE FORMULÁRIOS
+método get(padrão) ou post
+required = obrigatório
+minlength e maxlength = limitar os caracteres
+size = tamanho da caixa
+placeholder = dica
+autocomplete = sugestão
