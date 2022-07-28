9fcf69c16d23d055fa528db423dbe4b6ed7360b6
claudiostn <claudiostn26@gmail.com>
2022-07-28 15:04:09 -0300

    criei exercício 4 de formulários

--- curso-em-video/modulo4/exercicios/ex025/cadastro.php
+++ curso-em-video/modulo4/exercicios/ex025/cadastro.php
@@ -5,8 +5,15 @@ CONTROLES DE FORMULÁRIOS
 método get(padrão) ou post
 required = obrigatório
 minlength e maxlength = limitar os caracteres
+min e max = limitar o valor dos numeros
 size = tamanho da caixa
 placeholder = dica
 autocomplete = sugestão
 step = passo que um numero aumenta
 value = colocar um valor padrao na input
+pattern = padrão do input através de regex
+fieldset = tag para agrupar forms 
+
+
+o que estudar?
+regex expressoes regulares ^...$
